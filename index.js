73afdf6343cab63e9d3a435ad39de1771fcf3315
Timothy Shores <timothymshores@gmail.com>
2019-05-01 15:11:57 -0500

    Installed helmet via yarn and create index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hello World!');
+const express = require('express');
+const helmet = require('helmet');
+
+const studentsRouter = require('./routes/studentsRouter');
+const cohortsRouter = require('./routes/cohortsRouter');
+
+const server = express();
+server.use(express.json());
+server.use(helmet());
+
+server.use('/api/students', studentsRouter);
+server.use('/api/cohorts', cohortsRouter);
+
+const port = 5000;
+
+server.listen(port, () => {
+    console.log(`\n* Server Running on http://localhost:${port} *\n`);
+});
