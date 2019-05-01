69af8a953f68d11e6ffa768aa5a630ffc2dce186
Timothy Shores <timothymshores@gmail.com>
2019-05-01 17:08:14 -0500

    Had one on one Zoom with PM Angelo to create cohorts and students tables

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,12 @@
 module.exports = {
-    development: {
-        client: 'sqlite3',
-        connection: {
-            filename: './data/cohorts.db3'
-        },
-        useNullAsDefault: true
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/cohorts.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
     }
+  }
 };
--- routes/cohortsRouter.js
+++ routes/cohortsRouter.js
@@ -11,4 +11,10 @@ const knexConfig = {
 
 const db = knex(knexConfig);
 
+router.get('/', (req, res) => {
+    db('cohorts')
+        .then(name => res.status(200).json(name))
+        .catch(err => res.status(500).json(err));
+});
+
 module.exports = router;
