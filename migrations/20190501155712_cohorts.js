4cba6ce5c317ca99887d6e11b9e726f576ec4326
Timothy Shores <timothymshores@gmail.com>
2019-05-01 16:13:46 -0500

    Added schema to cohorts migration

--- migrations/20190501155712_cohorts.js
+++ migrations/20190501155712_cohorts.js
@@ -1,8 +1,11 @@
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('cohorts', tbl => {
+        tbl.increments();
+        tbl.string('name', 128).notNullable().unique();
+    })
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('cohorts');
 };
